8b0a3813fe837d591f0dc4ab083021472247de11
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-18 18:21:48 +0700

    menambahkan data searchnote

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -58,6 +58,18 @@ bool serachNode(int nim, Node* current, Node* previous) {
 		previous = current;
 		current = current->next;
 	}
-
+	
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 

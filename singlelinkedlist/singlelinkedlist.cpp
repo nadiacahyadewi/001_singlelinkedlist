0795428ac8bef9d7476215d33bec035af72e62b3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-05-18 17:45:56 +0700

    pemeriksaan nim yang sudah ada

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -34,5 +34,16 @@ void addnote() {
 
 	Node* previous = START;
 	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
 }
 
